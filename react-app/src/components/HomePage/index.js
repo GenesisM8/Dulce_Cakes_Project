ceea1904f3c5651bf1900541cf9f2f49cd04ee48
Genesis Mendes <genesismendes1@gmail.com>
2023-07-19 00:51:05 -0500

    home page has 3 imgs

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,40 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import './homePage.css'
-
-// const HomePage = () => {
-//     const images = [
-//         "https://www.frudeco.com/wp-content/uploads/2023/03/Frudeco-Feb-04-22.jpg",
-//         "https://www.frudeco.com/wp-content/uploads/2023/02/Frudeco-Feb-15-scaled.jpg",
-//     ];
-
-//     const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-//     useEffect(() => {
-//         // This function will switch the image index every 5 seconds
-//         const interval = setInterval(() => {
-//             setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-//         }, 5000);
-
-//         // Clean up the interval on unmount
-//         return () => clearInterval(interval);
-//     }, [images.length]);
-
-//     return (
-//         <div>
-//             <div className="img-container">
-//                 <img
-//                     src={images[currentImageIndex]}
-//                     alt="home-img"
-//                     className="home-img"
-//                 />
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default HomePage;
-
-
 
 import React, { useState, useEffect } from "react";
 import './homePage.css'
@@ -43,6 +6,7 @@ const HomePage = () => {
     const images = [
         "https://www.frudeco.com/wp-content/uploads/2023/03/Frudeco-Feb-04-22.jpg",
         "https://www.frudeco.com/wp-content/uploads/2023/02/Frudeco-Feb-15-scaled.jpg",
+        "https://www.frudeco.com/wp-content/uploads/2019/03/DSC00221-scaled.jpg"
     ];
 
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
