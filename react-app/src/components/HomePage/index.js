be58c85961d3c3a5aed9ea5307f5ad99d10974d1
Genesis Mendes <genesismendes1@gmail.com>
2023-07-19 00:39:51 -0500

    home page almost done

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -5,7 +5,6 @@
 //     const images = [
 //         "https://www.frudeco.com/wp-content/uploads/2023/03/Frudeco-Feb-04-22.jpg",
 //         "https://www.frudeco.com/wp-content/uploads/2023/02/Frudeco-Feb-15-scaled.jpg",
-       
 //     ];
 
 //     const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -36,7 +35,7 @@
 // export default HomePage;
 
 
-// HomePage.js
+
 import React, { useState, useEffect } from "react";
 import './homePage.css'
 
@@ -52,7 +51,7 @@ const HomePage = () => {
         // This function will switch the image index every 5 seconds
         const interval = setInterval(() => {
             setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
-        }, 5000);
+        }, 6000);
 
         // Clean up the interval on unmount
         return () => clearInterval(interval);
@@ -61,11 +60,14 @@ const HomePage = () => {
     return (
         <div>
             <div className="img-container">
-                <img
-                    src={images[currentImageIndex]}
-                    alt="home-img"
-                    className="home-img"
-                />
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image}
+                        alt="home-img"
+                        className={`home-img ${index === currentImageIndex ? "active" : "inactive"}`}
+                    />
+                ))}
             </div>
         </div>
     );
--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,62 +1,3 @@
-// import React from 'react';
-// import { NavLink } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-// import ProfileButton from './ProfileButton';
-// import './Navigation.css';
-// import logo from "../../assets/logo.png"
-// import background from "../../assets/background.png"
-
-// function Navigation({ isLoaded }) {
-//     const sessionUser = useSelector(state => state.session.user);
-//     const cartItems = useSelector((state) => state.cart.cartItems);
-//     const totalCartItems = cartItems.reduce((total, item) => total + item.quantity, 0);
-
-//     return (
-//         <div className='main-nav'>
-//             <div className='nav'>
-//                 <img src={logo} alt="Logo" className="logo" />
-//                 {/* <img src={background} alt='background'/> */}
-//                 <div className='icons'>
-//                     {isLoaded && (
-//                         <div>
-//                             <ProfileButton user={sessionUser} />
-//                         </div>
-//                     )}
-//                     <div>
-//                         <NavLink exact to="/cart">
-//                             <i className="fas fa-shopping-cart"></i>
-//                             {totalCartItems > 0 && <span className="cart-item-count">({totalCartItems})</span>}
-//                         </NavLink>
-//                     </div>
-//                 </div>
-//             </div>
-
-//             <div className='sub-nav-container'>
-//                 <div className='sub-nav'>
-//                     <NavLink exact to="/">Home</NavLink>
-//                     {/* Wrapping the cake-related links inside details element */}
-//                     <details className='order-dropdown'>
-//                         <summary className='order-nav'>Order</summary>
-// 						<div className='category-down'>
-// 						<NavLink exact to="/cakes">All Cakes</NavLink>
-//                         <NavLink exact to="/cakes/letters">Letter Cakes</NavLink>
-//                         <NavLink exact to="/cakes/numbers">Number Cakes</NavLink>
-//                         <NavLink exact to="/cakes/shapes">Shape Cakes</NavLink>
-//                         <NavLink exact to="/cakes/holiday">Holiday Cakes</NavLink>
-// 						</div>
-                        
-//                     </details>
-//                     <NavLink exact to="/reviews">Testimonials</NavLink>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Navigation;
-
-
-// Navigation.js
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
@@ -83,8 +24,10 @@ function Navigation({ isLoaded }) {
     return (
         <div className='main-nav'>
             <div className='nav'>
-                <img src={logo} alt="Logo" className="logo" />
-                {/* <img src={background} alt='background'/> */}
+				<NavLink exact to="/">
+					<img src={logo} alt="Logo" className="logo" />
+				</NavLink>
+                
                 <div className='icons'>
                     {isLoaded && (
                         <div>
@@ -102,14 +45,16 @@ function Navigation({ isLoaded }) {
 
             <div className='sub-nav-container'>
                 <div className='sub-nav'>
-                    <NavLink exact to="/">Home</NavLink>
-                    {/* Wrapping the cake-related links inside details element */}
+					<div >
+						<NavLink exact to="/" className='home-text' >HOME</NavLink>
+					</div>
+                   
                     <details
                         onMouseEnter={handleMenuOpen}
                         onMouseLeave={handleMenuClose}
                         open={isMenuOpen}
                     >
-                        <summary className='order-nav'>Order</summary>
+                        <summary>ORDER</summary>
                         <div className='category-down'>
                             <NavLink
                                 exact
@@ -148,7 +93,10 @@ function Navigation({ isLoaded }) {
                             </NavLink>
                         </div>
                     </details>
-                    <NavLink exact to="/reviews">Testimonials</NavLink>
+					<div>
+						<NavLink exact to="/reviews" className='home-text'>TESTIMONIALS</NavLink>
+					</div>
+                    
                 </div>
             </div>
         </div>
